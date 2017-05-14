9f1fca7a5427d181c04470258f7c0120cc58e088
Liu Cheng <alexnewtt@gmail.com>
2017-05-15 01:00:09 +0800

    xml parse
    Signed-off-by: Liu Cheng <alexnewtt@gmail.com>

--- test/utils/xmlUtils.js
+++ test/utils/xmlUtils.js
@@ -3,11 +3,69 @@
  */
 var fs = require('fs');
 var xml2js = require('xml2js');
+var async = require('async');
 
 var parser = new xml2js.Parser();
-fs.readFile(__dirname + '/HHQ-7_20151201092044.xml', function(err, data) {
-    parser.parseString(data, function (err, result) {
-        console.dir(result);
-        console.log('Done');
+
+function xml2json(file, callback) {
+    fs.readFile(file, function(err, data) {
+        parser.parseString(data, function (err, result) {
+            callback(result);
+        });
     });
-});
+}
+
+function readFileList(path, filesList) {
+    var files = fs.readdirSync(path);
+    files.forEach(function (itm, index) {
+        var stat = fs.statSync(path + itm);
+        if (stat.isDirectory()) {
+            //递归读取文件
+            readFileList(path + itm + "/", filesList)
+        } else {
+
+            var obj = {};//定义一个对象存放文件的路径和名字
+            obj.path = path;//路径
+            obj.filename = itm//名字
+            filesList.push(obj);
+        }
+
+    })
+
+}
+
+var getFiles = {
+    //获取文件夹下的所有文件
+    getFileList: function (path) {
+        var filesList = [];
+        readFileList(path, filesList);
+        return filesList;
+    },
+};
+
+var getXmls = {
+    getXmls: function (callback) {
+        var lists;
+        var tasks = [];
+        lists = getFiles.getFileList("../data/");
+
+        async.each(lists, function(itm, callback) {
+            xml2json(itm.path+itm.filename, function (result) {
+                // console.log(result);
+                tasks.push(result)
+                callback();
+            });
+        }, function(err){
+            console.log("err is:" + err);
+            callback(tasks);
+        })
+    },
+};
+
+{
+    getXmls.getXmls(function (tasks) {
+        console.log(tasks.toString());
+        console.log("Done");
+    });
+
+}
